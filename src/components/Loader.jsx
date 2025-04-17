b0abe9818d15ef7c3116f0d79d86035f1cfa4129
pepemaximusgame <pepemaximusgame@gmail.com>
2025-04-17 18:56:01 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,7 +5,7 @@ export const Loader = ()=>{
         <Html>
         <div className=" relative w-24 h-24  flex justify-center items-center">
         <div className="absolute loader animate-spin
-         w-12 h-12 border-t-4  border-red-500 
+         w-12 h-12 border-t-4 border-b-4 border-red-500 
          rounded-full"></div>
       <img className="w-12 h-19 z-10" src="images/pepeHigh.png"></img>
         </div>
